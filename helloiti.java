f9cb99172b3c7d76d5ce8239d770ec2f64ce84dc
essam <essameldenmohamed@yahoo.com>
2022-12-26 14:40:50 +0200

    edit helloiti

--- helloiti.java
+++ helloiti.java
@@ -1,7 +1,7 @@
 package mypkg;
 class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello "+ args[0]);
+		System.out.println("Hello itian");
 		System.out.println("printHelloGit");
 		
 		
