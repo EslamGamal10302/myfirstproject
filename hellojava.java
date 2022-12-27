70df374263da284d4e67f4bbbf1b603b8f541c7e
eslam <eslam10302@gmail.com>
2022-12-27 23:30:24 +0200

    my lasy modfiy

--- hellojava.java
+++ hellojava.java
@@ -3,5 +3,7 @@ class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello "+ args[0]);
 		System.out.println("Hello Essam ");
+		System.out.println("pull request ");
+		
 	}
 }
